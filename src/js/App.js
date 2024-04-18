d0a33889d95c71d351a153753ddec0ff2e13e2ac
LukeLay <lukelay.personal@gmail.com>
2024-04-18 10:35:36 -0400

    Update

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,3 @@
-//Before running, type "npm install" in a terminal to install all necessary packages.
-
-//To begin debuggin, type "npm run watch" and "npm start" in seperate terminals.
-
-//To compile and release, in a terminal type either of the following commands:
-//npx electron-packager "$(node -e "const path = require('path'); console.log(path.join(process.env.USERPROFILE, 'Documents', 'GitHub', '[YOUR_APP_NAME_HERE]'))")" [YOUR_APP_NAME_HERE] --platform=win32 --arch=x64 --electron-version=26.1.0 --overwrite
-//npx electron-packager "C:\Users\llay\Documents\GitHub\[YOUR_APP_NAME_HERE]" [YOUR_APP_NAME_HERE] --platform=win32 --arch=x64 --electron-version=26.1.0 --overwrite
-
 import React, { useEffect, useState } from "react";
 import { HashRouter, Link, Route, Routes } from "react-router-dom";
 
